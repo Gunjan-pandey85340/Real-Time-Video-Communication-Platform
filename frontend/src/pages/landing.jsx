1fc82046f8334cc31fa96507de17492f5cd95f80
Gunjan-pandey85340 <gunjanpandey8979@gmail.com>
2026-02-13 13:56:00 +0530

    final changes

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -8,7 +8,7 @@ export default function LandingPage() {
         <div className="landingPageContainer">
             <nav>
                 <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
+                    <h2>Real-Time Video Communication Platform</h2>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
@@ -30,7 +30,7 @@ export default function LandingPage() {
                 <div>
                     <h1><span style={{ color:"#FF9839" }}>Connect </span>with your loved once</h1>
 
-                    <p>Cover a distance by Apna Video Call</p>
+                    <p>Cover a distance by Connectify</p>
                     <div role="button">
                         <Link to={"/home"}>Get Started</Link>
                     </div>
